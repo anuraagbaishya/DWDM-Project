76e438e6759dbf9b72254579bf316d285e399522
Ishan Jain <jain.ishan95@gmail.com>
2017-03-30 17:21:15 +0530

    neat modular code

--- create_csv.py
+++ create_csv.py
@@ -1,5 +1,29 @@
 import pandas as pd
 
+
+def getfromfile(a):
+    returnvalue = []
+    for n in a:
+        returnvalue.append(n)
+    return returnvalue
+
+
+def uniquefn(uniquekey, col):
+    for n in col:
+        if n not in uniquekey:
+            uniquekey.append(n)
+    return uniquekey
+
+
+def process(uniquekey, col):
+    clusterfinal = []
+    for n in range(len(col)):
+        for x in range(len(uniquekey)):
+            if col[n] is uniquekey[x]:
+                clusterfinal.insert(n, x)
+    return clusterfinal
+
+
 fields = ["director_name", "actor_1_name", "actor_2_name", "actor_3_name", "plot_keywords", "gross"]
 dataset = pd.read_csv("movie_metadata.csv", usecols=fields)
 dataset = dataset.dropna(subset=["plot_keywords"])
@@ -7,19 +31,11 @@ dataset = dataset.dropna(subset=["gross"])
 plot_keywords = dataset.plot_keywords
 unique_plot_keywords = dict()
 
-dir_names = []
-act_1 = []
-act_2 = []
-act_3 = []
+dir_names = getfromfile(dataset.director_name)
+act_1 = getfromfile(dataset.actor_1_name)
+act_2 = getfromfile(dataset.actor_2_name)
+act_3 = getfromfile(dataset.actor_3_name)
 gross = []
-for n in dataset.director_name:
-    dir_names.append(n)
-for n in dataset.actor_1_name:
-    act_1.append(n)
-for n in dataset.actor_2_name:
-    act_2.append(n)
-for n in dataset.actor_3_name:
-    act_3.append(n)
 for n in dataset.gross:
     gross.append(str(n))
 data = []
@@ -29,9 +45,8 @@ filter ='with at from into during including until against among throughout despi
 following across behind beyond plus except but up out around down up out around down off above near'
 
 filter_words = filter.split(' ')
-
-
 index = 0
+
 for i in plot_keywords:
     plot_kword_final = []
     keywords = i.split("|")
@@ -54,76 +69,34 @@ for i in range(len(unique_plot_keywords)):
 
 df = pd.DataFrame(data)
 df.columns = ["director_name", "actor_1_name", "actor_2_name", "actor_3_name", "plot_keywords", "gross"]
-df.to_csv("new_data.csv")
-
-dirs, act1 = [], []
-act2, act3 = [], []
+df.to_csv("new_data1.csv")
 
 new_df = pd.read_csv("new_data.csv")
-for n in new_df.director_name:
-    dirs.append(n)
-
-for n in new_df.actor_1_name:
-    act1.append(n)
-
-for n in new_df.actor_2_name:
-    act2.append(n)
-
-for n in new_df.actor_3_name:
-    act3.append(n)
+dirs = getfromfile(new_df.director_name)
+act1 = getfromfile(new_df.actor_1_name)
+act2 = getfromfile(new_df.actor_2_name)
+act3 = getfromfile(new_df.actor_3_name)
 
 unique_dirs, dir_cluster = [], []
 unique_act1, act1_cluster = [], []
 unique_act2, act2_cluster = [], []
 unique_act3, act3_cluster = [], []
 cluster_data = []
-for n in dirs:
-    if n not in unique_dirs:
-        unique_dirs.append(n)
+unique_dirs = uniquefn(unique_dirs, dirs)
+unique_act1 = uniquefn(unique_act1, act1)
+unique_act2 = uniquefn(unique_act2, act2)
+unique_act3 = uniquefn(unique_act3, act3)
 
-for n in act1:
-    if n not in unique_act1:
-        unique_act1.append(n)
+dir_cluster = process(unique_dirs, dirs)
+act1_cluster = process(unique_act1, act1)
+act2_cluster = process(unique_act2, act2)
+act3_cluster = process(unique_act3, act3)
 
-for n in act3:
-    if n not in unique_act3:
-        unique_act3.append(n)
-
-for n in act2:
-    if n not in unique_act2:
-        unique_act2.append(n)
-
-print("Processing Dirs", len(dirs))
-
-for n in range(len(dirs)):
-    for x in range(len(unique_dirs)):
-        if dirs[n] is unique_dirs[x]:
-            dir_cluster.insert(n, x)
-
-
-print("Processing Act1", len(act1))
-
-for n in range(len(act1)):
-    for x in range(len(unique_act1)):
-        if act1[n] is unique_act1[x]:
-            act1_cluster.insert(n, x)
-
-print("Processing Act2", len(act2))
-
-for n in range(len(act2)):
-    for x in range(len(unique_act2)):
-        if act2[n] is unique_act2[x]:
-            act2_cluster.insert(n, x)
-
-print("Processing Act3", len(act3))
-
-for n in range(len(act3)):
-    for x in range(len(unique_act3)):
-        if act3[n] is unique_act3[x]:
-            act3_cluster.insert(n, x)
+print(dir_cluster)
+print(act1_cluster)
 
 for n in range(len(dir_cluster)):
     cluster_data.append((dir_cluster[n], act1_cluster[n], act2_cluster[n], act3_cluster[n], df.plot_keywords[n], df.gross[n]))
 df = pd.DataFrame(cluster_data)
 df.columns = ["director_name", "actor_1", "actor_2", "actor_3", "plot_keywords", "gross"]
-df.to_csv("cluster_data.csv")
+df.to_csv("cluster_data1.csv")
--- sort.py
+++ sort.py
@@ -1,110 +1,81 @@
 import pandas as pd
 from collections import defaultdict
+
+
+def getfromfile(a):
+    returnvalue = []
+    for n in a:
+        returnvalue.append(n)
+    return returnvalue
+
+
+def getcount(a):
+    hashcollection = defaultdict(list)
+    counted = []
+    for i in a:
+        count = 0
+        if i not in counted:
+            for col in a:
+                if i == col:
+                    count += 1
+            hashcollection[count].append(i)
+            counteddirectors.append(i)
+    return hashcollection
+
+
+def getcountactors(hashcollection, counted, a, act1, act2, act3):
+    for actor in a:
+        count = 0
+        if actor not in counted:
+            for col in act1:
+                if actor == col:
+                    count += 1
+            for col in act2:
+                if actor == col:
+                    count += 1
+            for col in act3:
+                if actor == col:
+                    count += 1
+            hashcollection[count].append(actor)
+            counted.append(actor)
+    return counted, hashcollection
+
+
+def putfrequency(hashmap1, col):
+    for n in range(len(col)):
+        count = 0
+        for key, value in hashmap1.iteritems():
+            if col[n] in value:
+                count = key
+                break
+        col[n] = count
+    return col
+
 hashmap = defaultdict(list)
 hashdirectors = defaultdict(list)
 fields = ["director_name", "actor_1_name", "actor_2_name", "actor_3_name", "plot_keywords", "gross"]
 dataset = pd.read_csv("new_data.csv", usecols=fields)
-act_1 = []
-act_2 = []
-act_3 = []
-dir_names = []
+act_1 = getfromfile(dataset.actor_1_name)
+act_2 = getfromfile(dataset.actor_2_name)
+act_3 = getfromfile(dataset.actor_3_name)
+dir_names = getfromfile(dataset.director_name)
 gross = []
-plot_keywords = []
+plot_keywords = getfromfile(dataset.plot_keywords)
 countedactors = []
 counteddirectors = []
 
-for n in dataset.plot_keywords:
-    plot_keywords.append(n)
-for n in dataset.director_name:
-    dir_names.append(n)
 for n in dataset.gross:
     gross.append(str(n))
-for n in dataset.actor_1_name:
-    act_1.append(n)
-for n in dataset.actor_2_name:
-    act_2.append(n)
-for n in dataset.actor_3_name:
-    act_3.append(n)
 
-for director in dir_names:
-    count = 0
-    if director not in counteddirectors:
-        for col in dir_names:
-            if director == col:
-                count += 1
-        hashdirectors[count].append(director)
-        counteddirectors.append(director)
-for actor in act_1:
-    count = 0
-    if actor not in countedactors:
-        for col in act_1:
-            if actor == col:
-                count += 1
-        for col in act_2:
-            if actor == col:
-                count += 1
-        for col in act_3:
-            if actor == col:
-                count += 1
-        hashmap[count].append(actor)
-        countedactors.append(actor)
-for actor in act_2:
-    count = 0
-    if actor not in countedactors:
-        for col in act_1:
-            if actor == col:
-                count += 1
-        for col in act_2:
-            if actor == col:
-                count += 1
-        for col in act_3:
-            if actor == col:
-                count += 1
-        hashmap[count].append(actor)
-        countedactors.append(actor)
-for actor in act_3:
-    count = 0
-    if actor not in countedactors:
-        for col in act_1:
-            if actor == col:
-                count += 1
-        for col in act_2:
-            if actor == col:
-                count += 1
-        for col in act_3:
-            if actor == col:
-                count += 1
-        hashmap[count].append(actor)
-        countedactors.append(actor)
+hashdirectors = getcount(dir_names)
+countedactors, hashmap=getcountactors(hashmap, countedactors, act_1, act_1, act_2, act_3)
+countedactors, hashmap=getcountactors(hashmap, countedactors, act_2, act_1, act_2, act_3)
+countedactors, hashmap=getcountactors(hashmap, countedactors, act_3, act_1, act_2, act_3)
 
-for n in range(len(dir_names)):
-    count = 0
-    for key, value in hashdirectors.iteritems():
-        if dir_names[n] in value:
-            count = key
-            break
-    dir_names[n] = count
-for n in range(len(act_1)):
-    count = 0
-    for key, value in hashmap.iteritems():
-        if act_1[n] in value:
-            count = key
-            break
-    act_1[n] = count
-for n in range(len(act_2)):
-    count = 0
-    for key, value in hashmap.iteritems():
-        if act_2[n] in value:
-            count = key
-            break
-    act_2[n] = count
-for n in range(len(act_3)):
-    count = 0
-    for key, value in hashmap.iteritems():
-        if act_3[n] in value:
-            count = key
-            break
-    act_3[n] = count
+dir_names = putfrequency(hashdirectors, dir_names)
+act_1 = putfrequency(hashmap, act_1)
+act_2 = putfrequency(hashmap, act_2)
+act_3 = putfrequency(hashmap, act_3)
 data = []
 print(act_1)
 for i in range(len(plot_keywords)):
@@ -112,7 +83,7 @@ for i in range(len(plot_keywords)):
     data.append(row)
 df = pd.DataFrame(data)
 df.columns = ["director_name", "actor_1_name", "actor_2_name", "actor_3_name", "plot_keywords", "gross"]
-df.to_csv("sorted_data.csv")
+df.to_csv("sorted_data1.csv")
 
 print("hello")
 print(hashmap)
